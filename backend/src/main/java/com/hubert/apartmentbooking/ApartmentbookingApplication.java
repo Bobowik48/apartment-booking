40b35ea5595e0c78936ea22e6a279151baa71e7b
bobowik48 <bobowik48@gmail.com>
2026-08-08 16:12:53 +0200

    Initialize backend (Spring Boot) and frontend (Angular) scaffolding

---
+++ backend/src/main/java/com/hubert/apartmentbooking/ApartmentbookingApplication.java
@@ -0,0 +1,13 @@
+package com.hubert.apartmentbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentbookingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hubert/apartmentbooking/ApartmentbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hubert.apartmentbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
